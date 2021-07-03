7d33e37a6f6881166b775970e9edfde09833d5c7
Jiaqi_Wang <Jiaqi_Wang@sjtu.edu.cn>
2021-07-03 10:40:38 +0000

    :smiley_cat: test more things together

--- hello/stl_vector_test.cc
+++ hello/stl_vector_test.cc
@@ -24,7 +24,7 @@
 
 #define FUN_AFTER(con, fun) do {                         \
   std::string fun_name = #fun;                           \
-  deallog << " After " << fun_name << " :\n";          \
+  deallog <<  fun_name << " : " << std::endl;          \
   fun;                                                   \
   COUT(con);                                             \
 } while(0)
@@ -35,7 +35,14 @@
   deallog << " " << con_name << " :";                  \
   for (auto it : container)                              \
     deallog << " " << it;                              \
-  deallog << "\n";                                     \
+  deallog << std::endl;                                   \
+} while(0)
+
+
+// 输出容器调用函数的值
+#define FUN_VALUE(fun) do {                              \
+  std::string fun_name = #fun;                           \
+  deallog << " " << fun_name << " : " << fun << std::endl;   \
 } while(0)
 
 void 
@@ -54,79 +61,67 @@ test()
   v9 = std::move(v3);
   v10 = { 1,2,3,4,5,6,7,8,9 };
 
-  // deallog << "Constructor: ";
-  // for (unsigned int i = 0; i < v1.size(); ++i)
-  //   deallog << v1[i] << " ";
-  // deallog << std::endl;
-
-  // v1.assign(8, 8);
-  // deallog << "AFTER v1.assign(8, 8) : \n v1 : ";
-  // for (unsigned int i = 0; i < v1.size(); ++i)
-  //   deallog << v1[i] << " ";
-  // deallog << std::endl;
-
-
-
-
-  FUN_AFTER(v1, v1.assign(8, 8));
+  FUN_AFTER(v1, v1.assign(a, a + 5));
+  FUN_AFTER(v1, v1.emplace(v1.begin(), 0));
+  FUN_AFTER(v1, v1.emplace_back(6));
+  FUN_AFTER(v1, v1.push_back(6));
+  FUN_AFTER(v1, v1.insert(v1.end(), 7));
+  FUN_AFTER(v1, v1.insert(v1.begin() + 3, 2, 3));
+  FUN_AFTER(v1, v1.insert(v1.begin(), a, a + 5));
+  FUN_AFTER(v1, v1.pop_back());
+  FUN_AFTER(v1, v1.erase(v1.begin()));
+  FUN_AFTER(v1, v1.erase(v1.begin(), v1.begin() + 2));
+  FUN_AFTER(v1, v1.reverse());
+  FUN_AFTER(v1, v1.swap(v4));
+  FUN_VALUE(*v1.begin());
+  FUN_VALUE(*(v1.end() - 1));
+  FUN_VALUE(*v1.rbegin());
+  FUN_VALUE(*(v1.rend() - 1));
+  FUN_VALUE(v1.front());
+  FUN_VALUE(v1.back());
+  FUN_VALUE(v1[0]);
+  FUN_VALUE(v1.at(1));
+  int* p = v1.data();
+  *p = 10;
+  *++p = 20;
+  p[1] = 30;
+  std::cout << " After change v1.data() :" << "\n";
+  COUT(v1);
+  std::cout << std::boolalpha;
+  FUN_VALUE(v1.empty());
+  std::cout << std::noboolalpha;
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.max_size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.resize(10));
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.shrink_to_fit());
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.resize(6, 6));
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.shrink_to_fit());
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.clear());
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.reserve(5));
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.reserve(20));
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
+  FUN_AFTER(v1, v1.shrink_to_fit());
+  FUN_VALUE(v1.size());
+  FUN_VALUE(v1.capacity());
 
   deallog << "OK" << std::endl;
-//   FUN_AFTER(v1, v1.assign(a, a + 5));
-//   FUN_AFTER(v1, v1.emplace(v1.begin(), 0));
-//   FUN_AFTER(v1, v1.emplace_back(6));
-//   FUN_AFTER(v1, v1.push_back(6));
-//   FUN_AFTER(v1, v1.insert(v1.end(), 7));
-//   FUN_AFTER(v1, v1.insert(v1.begin() + 3, 2, 3));
-//   FUN_AFTER(v1, v1.insert(v1.begin(), a, a + 5));
-//   FUN_AFTER(v1, v1.pop_back());
-//   FUN_AFTER(v1, v1.erase(v1.begin()));
-//   FUN_AFTER(v1, v1.erase(v1.begin(), v1.begin() + 2));
-//   FUN_AFTER(v1, v1.reverse());
-//   FUN_AFTER(v1, v1.swap(v4));
-//   FUN_VALUE(*v1.begin());
-//   FUN_VALUE(*(v1.end() - 1));
-//   FUN_VALUE(*v1.rbegin());
-//   FUN_VALUE(*(v1.rend() - 1));
-//   FUN_VALUE(v1.front());
-//   FUN_VALUE(v1.back());
-//   FUN_VALUE(v1[0]);
-//   FUN_VALUE(v1.at(1));
-//   int* p = v1.data();
-//   *p = 10;
-//   *++p = 20;
-//   p[1] = 30;
-//   std::cout << " After change v1.data() :" << "\n";
-//   COUT(v1);
-//   std::cout << std::boolalpha;
-//   FUN_VALUE(v1.empty());
-//   std::cout << std::noboolalpha;
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.max_size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.resize(10));
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.shrink_to_fit());
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.resize(6, 6));
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.shrink_to_fit());
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.clear());
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.reserve(5));
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.reserve(20));
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
-//   FUN_AFTER(v1, v1.shrink_to_fit());
-//   FUN_VALUE(v1.size());
-//   FUN_VALUE(v1.capacity());
+}
+
+
 //   PASSED;
 // #if PERFORMANCE_TEST_ON
 //   std::cout << "[--------------------- Performance Testing ---------------------]\n";
@@ -142,7 +137,7 @@ test()
 //   PASSED;
 // #endif
 //   std::cout << "[----------------- End container test : vector -----------------]\n";
-}
+
 
 
 
